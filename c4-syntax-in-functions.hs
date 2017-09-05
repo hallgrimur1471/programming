4b98221850961a745209f2bd202b03f256a2979a
hallgrimur1471 <hallgrimur1471@gmail.com>
2017-09-05 23:04:41 +0000

    Learn you a haskell

--- c4-syntax-in-functions.hs
+++ c4-syntax-in-functions.hs
@@ -7,3 +7,69 @@ head' (x:_) = x
 length' :: (Num b) => [a] -> b
 length' [] = 0
 length' (_:xs) = 1 + length' xs
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+
+capital :: String -> String
+capital "" = "Empty string, whoops!"
+capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+
+bmiTell :: (RealFloat a) => a -> String
+bmiTell bmi
+    | bmi <= 18.5 = "You're underweight, you emo, you!"
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= 30.0 = "You're fat! Lose some wiehgt, fatty!"
+    | otherwise   = "You're a whale, congratulations!"
+
+bmiTell2 :: (RealFloat a) => a -> a -> String
+bmiTell2 weight height
+    | weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"
+    | weight / height ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | weight / height ^ 2 <= 30.0 = "You're fat! Lose some wiehgt, fatty!"
+    | otherwise   = "You're a whale, congratulations!"
+
+bmiTell3 :: (RealFloat a) => a -> a -> String
+bmiTell3 weight height
+    | bmi <= skinny = "You're underweight, you emo, you!"
+    | bmi <= normal = "You're supposedly normal. Pffft, I bet you're ugly!"
+    | bmi <= fat    = "You're fat! Lose some wiehgt, fatty!"
+    | otherwise     = "You're a whale, congratulations!"
+    where bmi = weight / height ^ 2
+          (skinny, normal, fat) = (18.5, 25.0, 30.0)
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis xs = [bmi w h | (w, h) <- xs]
+    where bmi weight height = weight / height ^ 2
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+    | a > b     = GT
+    | a == b    = EQ
+    | otherwise = LT
+
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h =
+    let sideArea = 2 * pi * r * h
+        topArea = pi * r^2
+    in  sideArea + 2 * topArea
+
+head1' :: [a] -> a
+head1' []    = error "No head for empty lists!"
+head1' (x:_) = x
+
+head2' :: [a] -> a
+head2' xs = case xs of []    -> error "No head for empty lists!"
+                       (x:_) -> x
+
+describeList1 :: [a] -> String
+describeList1 xs = "The list is " ++ case xs of [] -> "empty."
+                                                [x] -> "a singleton list."
+                                                xs -> "a longer list."
+
+describeList2 :: [a] -> String
+describeList2 xs = "The list is " ++ what xs
+    where what [] = "empty."
+          what [x] = "a singleton list."
+          what xs = "a longer list."

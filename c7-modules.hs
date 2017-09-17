04e44b76f4bdb484cf9bb8ab9adf80a14701e23d
hallgrimur1471 <hallgrimur1471@gmail.com>
2017-09-17 12:53:25 +0000

    Learn you a haskell

---
+++ c7-modules.hs
@@ -0,0 +1,10 @@
+import qualified Geometry.Sphere as Sphere
+import qualified Geometry.Cuboid as Cuboid
+import qualified Geometry.Cube as Cube
+
+a = Sphere.volume 2
+b = Sphere.area 2
+c = Cuboid.volume 1 2 3
+d = Cuboid.area 1 2 3
+e = Cube.volume 2
+f = Cube.area 2

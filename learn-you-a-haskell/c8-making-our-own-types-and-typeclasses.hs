b22de911495e79fa59d58d04cc71621b8b21436c
hallgrimur1471 <hallgrimur1471@gmail.com>
2017-11-14 09:15:18 +0000

    Learn you a haskell

--- learn-you-a-haskell/c8-making-our-own-types-and-typeclasses.hs
+++ learn-you-a-haskell/c8-making-our-own-types-and-typeclasses.hs
@@ -1,3 +1,5 @@
+import qualified Data.Map as Map
+
 module Shapes
 ( Point(..)
 , Shape(..)
@@ -36,3 +38,27 @@ vectMult :: (Num t) => Vector t -> t -> Vector t
 
 scalarMult :: (Num t) => Vector t -> Vector t -> t
 (Vector i j k) `scalarMult` (Vector l m n) = i*l + j*m + k*n
+
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
+           deriving (Eq, Ord, Show, Read, Bounded, Enum)
+
+-- read "Saturday" :: Day
+-- Monday `compare` Wednesday 
+-- maxBound :: Day
+-- succ Monday
+-- [Thursday .. Sunday]
+-- [minBound .. maxBound] :: [Day]
+
+data LockerState = Taken | Free deriving (Show, Eq)
+type Code = String
+type LockerMap = Map.Map Int (LockerState, Code)
+
+lockerLookup :: Int -> LockerMap -> Either String Code
+lockerLookup lockerNumber map =
+    case Map.lookup lockerNumber map of
+        Nothing -> Left $ "Locker number " ++ show lockerNumber ++ " doesn't exist!"
+        Just (state, code) -> if state /= Taken
+                                then Right code
+                                else Left $ "Locker " ++ show lockerNumber ++ " is already taken!"
+
+

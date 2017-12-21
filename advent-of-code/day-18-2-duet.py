13ae1a129e9b29befd6d12145ca04d30110f045c
hallgrimur1471 <hallgrimur1471@gmail.com>
2017-12-21 15:19:17 +0000

    Advent of code

--- advent-of-code/day-18-2-duet.py
+++ advent-of-code/day-18-2-duet.py
@@ -9,37 +9,32 @@ class Program:
     def __init__(self, instructions):
         self.instructions = instructions
         self.registers = dict()
-        self._is_waiting_for_data = False
         self.is_terminated = False
         self.num_sent_packets = 0
         self._instruction_pointer = 0
         self._instruction_register =\
                 self.instructions[self._instruction_pointer]
-        self._pipe = None
-        self._received_values = []
+        self._pipe_in = []
+        self._pipe_out = []
         self._jumped_on_last_instruction = False
 
     def step(self):
         self._execute_current_instruction()
-        if not (self._jumped_on_last_instruction or self._is_waiting_for_data):
+        if not (self._jumped_on_last_instruction or self.is_waiting_for_data()):
             self._move_to_next_instruction()
+        self._jumped_on_last_instruction = False
 
-    def set_pipe_to(self, program):
-        self._pipe = program
-
-    def receive_through_input(self, value):
-        self._received_values.insert(0, value)
-
-    def value_of_register(self, register):
-        if register not in self.registers:
-            self._initialize_register(register)
-        return self.registers[register]
+    def pipe_output_to(self, program):
+        if not isinstance(program, Program):
+            error("pipe_output_to can only pipe to another program")
+        self._pipe_out = program._pipe_in
 
     def set_register(self, register, value):
         self.registers[register] = value
 
     def is_waiting_for_data(self):
-        return self._received_values == [] and self._is_waiting_for_data
+        return (len(self._pipe_in) == 0 and
+                self._instruction_register.operation== "rcv")
 
     def _execute_current_instruction(self):
         instruction = self._instruction_register
@@ -51,21 +46,16 @@ class Program:
         if first_argument.isalpha() and first_argument not in self.registers:
             self._initialize_register(first_argument)
         if operation == "snd":
-            self._send_to_pipe(self._evaluate(first_argument))
+            self._send_value(self._evaluate(first_argument))
             self.num_sent_packets += 1
         elif operation == "rcv":
-            if self._received_values:
-                self.registers[first_argument] = self._received_values.pop()
-            else:
-                self._is_waiting_for_data = True
-                return
+            if len(self._pipe_in) > 0:
+                self.registers[first_argument] = self._pipe_in.pop()
         elif operation == "jgz":
             first_argument_value = self._evaluate(first_argument)
             if first_argument_value > 0:
                 self._jump(second_argument_value)
-                return
-            else:
-                pass
+                self._jumped_on_last_instruction = True
         elif operation == "set":
             self.registers[first_argument] = second_argument_value
         elif operation == "add":
@@ -74,25 +64,20 @@ class Program:
             self.registers[first_argument] *= second_argument_value
         elif operation == "mod":
             self.registers[first_argument] %= second_argument_value
-        self._jumped_on_last_instruction = False
-        self._is_waiting_for_data = False
+
+    def _send_value(self, value):
+        self._pipe_out.insert(0, value)
 
     def _move_to_next_instruction(self):
         self._jump(1)
 
-    def _send_to_pipe(self, value):
-        if self._pipe is None:
-            error("pipe not set, can not send value")
-        self._pipe.receive_through_input(value)
-
     def _jump(self, jump_size):
         self._instruction_pointer += jump_size
         if self._instruction_pointer_is_out_of_bounds():
-            self.is_termianted = True
+            self.is_terminated = True
             return
         self._instruction_register =\
                 self.instructions[self._instruction_pointer]
-        self._jumped_on_last_instruction = True
 
     def _instruction_pointer_is_out_of_bounds(self):
         return (self._instruction_pointer < 0 or 
@@ -117,8 +102,8 @@ class Program:
 class Instruction:
     def __init__(self, operation, first_argument, second_argument=None):
         self.operation = operation
-        self.first_argument = first_argument # register
-        self.second_argument = second_argument # number or register
+        self.first_argument = first_argument
+        self.second_argument = second_argument
 
     def len(self):
         if self.second_argument == None:
@@ -126,9 +111,9 @@ class Instruction:
         else:
             return 3
 
-    def print(self):
-        return (str(self.operation)+" "+str(self.first_argument)
-                +" "+str(self.second_argument))
+    def to_str(self):
+        return " ".join(map(str, [self.operation, self.first_argument,
+                self.second_argument]))
 
 def main():
     start_time = time.time()
@@ -146,26 +131,22 @@ def main():
     B = Program(instructions)
     A.set_register('p', 0)
     B.set_register('p', 1)
-    A.set_pipe_to(B)
-    B.set_pipe_to(A)
+    A.pipe_output_to(B)
+    B.pipe_output_to(A)
     deadlock = False
-    finished = False
-    while not (deadlock or finished):
+    while not deadlock:
         while not (A.is_waiting_for_data() or A.is_terminated):
             A.step()
         while not (B.is_waiting_for_data() or B.is_terminated):
             B.step()
-        if A.is_waiting_for_data() and B.is_waiting_for_data():
-            print("deadlock")
+        if ((A.is_waiting_for_data() or A.is_terminated) and
+            (B.is_waiting_for_data() or B.is_terminated)):
             deadlock = True
-        if A.is_terminated and B.is_terminated:
-            print("finished")
-            finished = True
     print("Program B sent "+str(B.num_sent_packets)+" packets.")
 
 def print_debug_info(msg, A, B):
-    print(msg+" | A:"+str(A.registers), str(A._received_values)
-                  +", B:"+str(B.registers), str(B._received_values))
+    print(msg+" | A:"+str(A.registers), "str(A._pipe_in)"
+                  +", B:"+str(B.registers), "str(B._pipe_in)")
 
 def seconds_to_hms(seconds):
     m, s = divmod(seconds, 60)

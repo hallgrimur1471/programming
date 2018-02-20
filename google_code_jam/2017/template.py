def952ad6351c8c390170ccc3d3cf79ca0c9c1f4
hallgrimur1471 <hallgrimur1471@gmail.com>
2018-02-20 22:52:56 +0000

    Google code jam

---
+++ google_code_jam/2017/template.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from sys import stdin, stdout, stderr
+import sys
+
+sys.setrecursionlimit(10000)
+
+def main():
+    pass
+
+def solve():
+    pass
+
+def pt(*args):
+    print(*args, file=stderr)

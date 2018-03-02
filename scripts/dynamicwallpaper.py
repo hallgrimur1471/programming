c295d5448f4c8589a1e2e5fe42c3b0d3e307f8ad
hallgrimur1471 <hallgrimur1471@gmail.com>
2018-03-02 13:27:26 +0000

    Scripts

--- scripts/dynamicwallpaper.py
+++ scripts/dynamicwallpaper.py
@@ -4,9 +4,8 @@ from PIL import Image, ImageDraw, ImageFont
 from random import randint
 import commands
 
-
-# get an image
-base = Image.open('/home/hallgrimur1471/bashscripts/dynamicwallpaper_base.jpg').convert('RGBA')
+# get a base image
+base = Image.open('/home/hallgrimur1471/programming/scripts/dynamicwallpaper_base.jpg').convert('RGBA')
 imageWidth, imageHeight = base.size
 
 # make a blank image for the text, initialized to transparent text color
@@ -17,27 +16,46 @@ fnt = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 40)
 # get a drawing context
 d = ImageDraw.Draw(txt)
 
-n = 3
+n = 7 # number of projects
+
 project1 = "The C Programming Language"
 project1TotalExercises = commands.getstatusoutput('cat /home/hallgrimur1471/c/exercise-count.txt | awk \'{sum += $3} END {print sum}\'')[1]
 project1CurrentExercise = commands.getstatusoutput('ls /home/hallgrimur1471/c | egrep \'^ex.-.*\.c$\' | wc -l')[1]
 progress1 = 100*float(project1CurrentExercise)/(int(project1TotalExercises)+1)
 progress1 = "%.1f%%" % progress1
+
 project2 = "Learn you a Haskell"
 project2TotalChapters = 14
 project2CurrentChapter = commands.getstatusoutput('ls /home/hallgrimur1471/programming/learn-you-a-haskell | egrep \'^c[0-9]+-.*\.hs$\' | egrep -o \'^c[0-9]+-\' | uniq | wc -l')[1]
 progress2 = 100*float(project2CurrentChapter)/(int(project2TotalChapters)+1)
 progress2 = "%.1f%%" % progress2
+
 project3 = "Scheme interpreter written in Haskell"
 project3TotalChapters = commands.getstatusoutput('cat /home/hallgrimur1471/programming/scheme-interpreter-written-in-haskell/project.progress | wc -l')[1]
 project3CurrentChapter = commands.getstatusoutput('cat /home/hallgrimur1471/programming/scheme-interpreter-written-in-haskell/project.progress | awk \'{sum += $2} END {print sum}\'')[1]
 progress3 = 100*float(project3CurrentChapter)/(int(project3TotalChapters))
 progress3 = "%.1f%%" % progress3
 
-project4 = "Fast reading course"
-project5 = "Linear regression"
-project6 = "Cryptopals"
-project7 = "Advent of code"
+project4 = "Matasano crypto challenges"
+project4TotalChallenges = 64+1 # +1 so we don't go to 100% when starting last challenge
+project4CurrentChallenge = commands.getstatusoutput("cd /home/hallgrimur1471/programming/matasano_crypto_challenges/matasano_cryptography && ls | egrep '^s[0-9]_.+$' | xargs ls -1 | egrep -o '^c[0-9][0-9]' | uniq | wc -l")[1]
+progress4 = 100*float(project4CurrentChallenge)/(int(project4TotalChallenges))
+progress4 = "%.1f%%" % progress4
+
+project5 = "Advent of code"
+project5TotalChallenges = 25*2+1 # +1 so we don't go to 100% when starting last challenge
+project5CurrentChallenge = commands.getstatusoutput("cd /home/hallgrimur1471/programming/advent-of-code && ls | egrep '^day(-|_)[0-9]+.*\.(py|hs)$' | wc -l")[1]
+progress5 = 100*float(project5CurrentChallenge)/(int(project5TotalChallenges))
+progress5 = "%.1f%%" % progress5
+
+project6 = "Neural networks"
+project6TotalCourses = 15
+project6CompletedCourses = 1
+progress6 = 100*float(project6CompletedCourses)/(int(project6TotalCourses))
+progress6 = "%.1f%%" % progress6
+
+project7 = "Fast reading course"
+progress7 = "35.00%%"
 
 fx = float(imageWidth)/2
 vs = imageHeight/(n+2)
@@ -53,15 +71,14 @@ color2 = 255
 color3 = 255
 textColor = (color1,color2,color3,255)
 
-d.text((fx-12*len(project1),1*vs), project1, font=fnt, fill=textColor)
-d.text((fx-12*len(progress1),1*vs+60), progress1, font=fnt, fill=textColor)
-
-d.text((fx-12*len(project2),2*vs), project2, font=fnt, fill=textColor)
-d.text((fx-12*len(progress2),2*vs+60), progress2, font=fnt, fill=textColor)
-
-d.text((fx-12*len(project3),3*vs), project3, font=fnt, fill=textColor)
-d.text((fx-12*len(progress3),3*vs+60), progress3, font=fnt, fill=textColor)
+projects = [project1, project2, project3, project4, project4, project6, project7]
+progresses = [progress1, progress2, progress3, progress4, progress5, progress6, progress7]
+for i in range(n):
+    d.text((fx-12*len(projects[i]),(i+1)*vs), projects[i], font=fnt, fill=textColor)
+    d.text((fx-12*len(progresses[i]),(i+1)*vs+60), progresses[i], font=fnt, fill=textColor)
 
 out = Image.alpha_composite(base, txt)
-out.save("/home/hallgrimur1471/bashscripts/dynamicwallpaper.jpg", "jpeg")
+
+# this will automatically update the background since gsettings knows this is the background file
+out.save("/home/hallgrimur1471/programming/scripts/dynamicwallpaper.jpg", "jpeg")
 
--- scripts/dynamicwallpaper.sh
+++ scripts/dynamicwallpaper.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 pic='/home/hallgrimur1471/Pictures/dynamicWallpaper.jpg'
+pic2='/home/hallgrimur1471/programming/scripts/dynamicwallpaper.jpg'
+pic3='/home/hallgrimur1471/Pictures/city_melt_by_burningmonk-d5grkur.jpg'
 
-gsettings set org.gnome.desktop.background picture-uri file://$pic
+gsettings set org.gnome.desktop.background picture-uri file://$pic2

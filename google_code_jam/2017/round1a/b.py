bd0f92dda718fe1bec8a76ac31f8e2831d1b12b1
hallgrimur1471 <hallgrimur1471@gmail.com>
2018-03-18 12:59:40 +0000

    Google code jam

--- google_code_jam/2017/round1a/b.py
+++ google_code_jam/2017/round1a/b.py
@@ -2,7 +2,10 @@
 
 from sys import stdin, stdout, stderr
 import sys
-from math import floor
+from math import floor, ceil
+
+import numpy as np
+from numpy import zeros
 
 sys.setrecursionlimit(10000)
 
@@ -15,19 +18,41 @@ def main():
         for i in range(N):
             Q.append(list(map(int, input().split(' '))))
         s = solve(N, P, R, Q)
+        pt("Case #{}: {}".format(t+1, s))
         print("Case #{}: {}".format(t+1, s))
 
 def solve(N, P, R, Q):
-    u = []
+    m = zeros(shape=(N,P), dtype=int).tolist()
     for i in range(N):
-        u.append([0]*P)
-    for ig in range(P):
-        s = []
-        r = floor(Q[0][ig]*0.9/R[0])
-        while r*R[0] <= Q[0][ig]*1.1:
-            if r*R[0] >= Q[0][ig]*0.9:
-                s.append(r)
+        for j in range(P):
+            mn = ceil((10/11)*Q[i][j]/R[i])
+            mx = floor((10/9)*Q[i][j]/R[i])
+            m[i][j] = (mn, mx)
+    rv = zeros(shape=(N,P), dtype=int).tolist()
+    m = list(map(sorted, m))
+    c = 0
+    def rd(i, j):
+        nonlocal c, rv
+        u = m[i][j]
+        if u[0] > u[1]:
+            return
+        if i == N-1:
+            c += 1
+            return
+        r = 0
+        while True:
+            if rv[i+1][r] == 0:
+                v = m[i+1][r]
+                if u[1] >= v[0] and u[0] <= v[1]:
+                    break
             r += 1
+            if r >= P:
+                return
+        rv[i+1][r] = 1
+        rd(i+1, r)
+    for j in range(P):
+        rd(0, j)
+    return c
 
 def pt(*args):
     print(*args, file=stderr)

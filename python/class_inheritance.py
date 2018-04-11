6868146859f709f51e3bd42aeea27d04b81ca987
hallgrimur1471 <hallgrimur1471@gmail.com>
2018-04-11 18:22:06 +0000

    Add class inheritance test

---
+++ python/class_inheritance.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+class A:
+    def __init__(self):
+        pass
+
+    def launch(self):
+        #
+        # a lot of crazy lines
+        # doing crazy stuff
+        print("Tear the roof off the sucker")
+        self.funk()
+    
+    def funk(self):
+        print("We want the funk")
+
+class B(A):
+    def __init__(self):
+        pass
+
+    def launch(self):
+        #
+        # do some thing new here
+        #
+
+        #
+        # now we want to run all the "crazy lines" in A.launch
+        # but when self.funk() is called we want to run B.funk
+        # how do you do that?
+        a = super()
+        a.funk = self.funk
+        a.launch()
+
+    def funk(self):
+        print("Give up the funk")
+
+b = B()
+b.launch()
+
+# desired output:
+#     Tear the roof off the sucker
+#     Give up the funk

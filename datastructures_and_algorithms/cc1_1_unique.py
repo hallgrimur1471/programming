903e5038a01cb791a3d0ce79a632da11e33ac9f0
hallgrimur1471 <hallgrimur1471@gmail.com>
2018-08-19 11:26:36 +0000

    Datastructures and algorithms

--- datastructures_and_algorithms/cc1_1_unique.py
+++ datastructures_and_algorithms/cc1_1_unique.py
@@ -5,7 +5,7 @@ Implement an algorithm to determine if a string has all unique characters.
 What if you cannot use additional data structures?
 """
 
-# O(n) using dict
+# O(n) using hash-table (Python dict)
 def is_unique(s):
     k = {}
     for c in s:
@@ -14,7 +14,17 @@ def is_unique(s):
         k[c] = "has_occured"
     return True
 
-print(is_unique("abcde"))
-print(is_unique("abcbe"))
+# A hash-set would suite better ... (Python set)
+# O(n)
+def is_unique2(s):
+    hs = set()
+    for c in s:
+        if c in hs:
+            return False
+        hs.add(c)
+    return True
 
-# if arrays and dicts are forbidden we could use double for loop O(n**2)
+print(is_unique2("abcde"))
+print(is_unique2("abcbe"))
+
+# if lists, dicts and sets are forbidden we could use double for loop O(n**2)

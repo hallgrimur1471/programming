7f46d1c87bdbbf70623fcb5ba389c39da30a0e09
hallgrimur1471 <hallgrimur1471@gmail.com>
2019-08-16 17:36:29 +0000

    Pwnable.kr

--- pwnable/collision/pwn_col.py
+++ pwnable/collision/pwn_col.py
@@ -2,7 +2,11 @@
 
 import subprocess
 
-for i in range(0, 1):
-    cmd = ["./print_input_info", "\xfff6"]
-    out = subprocess.run(cmd, stdout=subprocess.PIPE)
-    print(i, out.stdout.decode().rstrip())
+colliding_password = (
+    "54 15 21 115 -67 -31 -67 -62 -44 -2 120 "
+    + "-15 -102 -10 -1 -110 -117 35 -111 103"
+)  # found using find_collission.c
+
+cmd = f"./col `../utils/print_bytes {colliding_password}`"
+print(cmd)
+subprocess.run(cmd, shell=True)

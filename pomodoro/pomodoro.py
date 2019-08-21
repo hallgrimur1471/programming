fd554d821d6e17b05113f5b51d382041455ad9b1
hallgrimur1471 <hallgrimur1471@gmail.com>
2019-08-21 22:24:49 +0000

    Refactor code

--- pomodoro/pomodoro.py
+++ pomodoro/pomodoro.py
@@ -7,19 +7,17 @@ import playsound
 
 
 def main():
-    if should_be_working_now():
-        tprint("Waiting until next break ...")
-        wait_until_break()
-        run_break_alert()
-
-    while True:
-        tprint("Waiting until break is over ...")
+    if not should_be_working_now():
         wait_until_break_is_over()
         run_break_over_alert()
 
-        tprint("Waiting until next break ...")
+    while True:
         wait_until_break()
         run_break_alert()
+
+        wait_until_break_is_over()
+        run_break_over_alert()
+
 
 
 def should_be_working_now():
@@ -35,24 +33,25 @@ def should_be_working_during(minute):
     return (5 <= minute and minute < 30) or (35 <= minute)
 
 
+
+def wait_until_break_is_over():
+    tprint("Waiting until break is over ...")
+    wait_until(lambda: should_be_working_now())
+
+
 def tprint(*args, **kwargs):
     current_time = time.localtime()
     current_time_str = time.strftime("%H:%M:%S", current_time)
     print("[{}]:".format(current_time_str), *args, **kwargs)
 
-
-def wait_until_break():
-    wait_until(lambda: not should_be_working_now())
-
-
 def wait_until(criteria_is_met):
     while not criteria_is_met():
         time.sleep(0.2)
 
+def run_break_over_alert():
+    tprint("It's time to start working again!")
+    play_sound("airship_clear.mp3")
 
-def run_break_alert():
-    tprint("It's time for a break!")
-    play_sound("stage_clear.mp3")
 
 
 def play_sound(sound_file_name):
@@ -67,13 +66,14 @@ def get_project_root_dir():
     return project_root_dir
 
 
-def wait_until_break_is_over():
-    wait_until(lambda: should_be_working_now())
+def wait_until_break():
+    tprint("Waiting until next break ...")
+    wait_until(lambda: not should_be_working_now())
 
 
-def run_break_over_alert():
-    tprint("It's time to start working again!")
-    play_sound("airship_clear.mp3")
+def run_break_alert():
+    tprint("It's time for a break!")
+    play_sound("stage_clear.mp3")
 
 
 if __name__ == "__main__":

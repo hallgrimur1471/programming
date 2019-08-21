73db4adee5e5211a9952bbb29b2f2a0aa06f009e
hallgrimur1471 <hallgrimur1471@gmail.com>
2019-08-21 22:20:39 +0000

    Refactor code

--- pomodoro/pomodoro.py
+++ pomodoro/pomodoro.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 
 import time
+import os.path
 
-from playsound import playsound
+import playsound
 
 
 def main():
-    if next_event_is_break():
+    if should_be_working_now():
         tprint("Waiting until next break ...")
         wait_until_break()
         run_break_alert()
@@ -21,46 +22,58 @@ def main():
         run_break_alert()
 
 
-def next_event_is_break():
+def should_be_working_now():
     minute = get_current_minute()
     return should_be_working_during(minute)
 
 
 def get_current_minute():
-    current_minute = time.localtime().tm_min
-    return current_minute
+    return time.localtime().tm_min
 
 
 def should_be_working_during(minute):
-    if (5 <= minute and minute < 30) or (35 <= minute):
-        return True
-    return False
-
-
-def wait_until_break():
-    while should_be_working_during(get_current_minute()):
-        time.sleep(0.2)
-
-
-def wait_until_break_is_over():
-    while not should_be_working_during(get_current_minute()):
-        time.sleep(0.2)
-
-
-def run_break_alert():
-    tprint("It's time for a break!")
-    playsound("sounds/stage_clear.mp3")
-
-
-def run_break_over_alert():
-    tprint("It's time to start working again!")
-    playsound("sounds/airship_clear.mp3")
+    return (5 <= minute and minute < 30) or (35 <= minute)
 
 
 def tprint(*args, **kwargs):
     current_time = time.localtime()
     current_time_str = time.strftime("%H:%M:%S", current_time)
-    print("[{}] ".format(current_time_str), *args, **kwargs)
+    print("[{}]:".format(current_time_str), *args, **kwargs)
+
+
+def wait_until_break():
+    wait_until(lambda: not should_be_working_now())
+
+
+def wait_until(criteria_is_met):
+    while not criteria_is_met():
+        time.sleep(0.2)
+
+
+def run_break_alert():
+    tprint("It's time for a break!")
+    play_sound("stage_clear.mp3")
+
+
+def play_sound(sound_file_name):
+    project_root_dir = get_project_root_dir()
+    playsound.playsound(
+        os.path.join(project_root_dir, "sounds/{}".format(sound_file_name))
+    )
+
+
+def get_project_root_dir():
+    project_root_dir = os.path.dirname(os.path.abspath(__file__))
+    return project_root_dir
+
+
+def wait_until_break_is_over():
+    wait_until(lambda: should_be_working_now())
+
+
+def run_break_over_alert():
+    tprint("It's time to start working again!")
+    play_sound("airship_clear.mp3")
 
 
 if __name__ == "__main__":

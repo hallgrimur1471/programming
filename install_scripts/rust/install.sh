7c44d8ceeb0e3204445a9e2e803d0d597b8d4172
Hallgrimur David Egilsson <hallgrimur1471@gmail.com>
2019-08-31 16:55:37 +0000

    Add formatting and linting tool

--- install_scripts/rust/install.sh
+++ install_scripts/rust/install.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
+# Install cargo, rustc and rustup
 curl https://sh.rustup.rs -sSf | sh
+
+# Add formatting tool
+rustup component add rustfmt
+
+# Add linting tool
+rustup component add clippy

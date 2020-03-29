5d601874dc8c022bf0cec0fee0838902893921b7
hallgrimur1471 <hallgrimur1471@gmail.com>
2020-03-29 19:22:18 +0000

    Google code jam

---
+++ google_code_jam/2020/practice/2019/round1a/b1.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define watch(x) cerr << (#x) << " is " << (x) << endl
+
+// Begin: divisors are all pairwise coprime, and sorted in descending order
+template <class T>
+int use_chineese_remainder_theorem(T divisors_begin, T divisors_end,
+                                   T remainders_begin, T remainders_end) {
+  auto dc_it = divisors_begin;
+  auto rc_it = remainders_begin;
+  int x = (*rc_it);
+  while (true) {
+    while ((dc_it != divisors_end) && ((x % (*dc_it)) == (*rc_it))) {
+      dc_it++;
+      rc_it++;
+    }
+    if (dc_it == divisors_end) {
+      break;
+    } else {
+      dc_it = divisors_begin;
+      rc_it = remainders_begin;
+      x += (*dc_it);
+    }
+  }
+  return x;
+}
+
+void solve(int T, int N, int M) {
+  array<int, 6> bc = {18, 17, 13, 11, 7, 5};
+  array<int, 6> rs;
+
+  for (int n = 0; n < 6; n++) {
+    for (int i = 0; i < 18; i++) {
+      cout << bc[n] << " ";
+    }
+    cout << endl;
+    array<int, 18> brs;
+    brs.fill(0);
+    for (int i = 0; i < 18; i++) {
+      int br;
+      cin >> br;
+      if (br == -1) {
+        exit(1);
+      } else {
+        brs[i] = br;
+      }
+    }
+    int r = accumulate(brs.begin(), brs.end(), 0) % bc[n];
+    ;
+    rs[n] = r;
+  }
+
+  int G = use_chineese_remainder_theorem(bc.begin(), bc.end(), rs.begin(),
+                                         rs.end());
+  cout << G << endl;
+  int ans;
+  cin >> ans;
+  if (ans == -1) {
+    exit(1);
+  }
+}
+
+int main() {
+  int T, N, M;
+  cin >> T >> N >> M;
+  watch(M);
+  for (int t = 1; t <= T; t++) {
+    solve(T, N, M);
+  }
+  return 0;
+}
--- google_code_jam/2020/template.cpp
+++ google_code_jam/2020/template.cpp
@@ -4,6 +4,18 @@ using namespace std;
 
 #define watch(x) cout << (#x) << " is " << (x) << endl
 
+#define OPI(v)                                                                 \
+  for (auto e : v) {                                                           \
+    cout << e << " ";                                                          \
+  }                                                                            \
+  cout << "\n";
+
+#define EPI(v)                                                                 \
+  for (auto e : v) {                                                           \
+    cerr << e << " ";                                                          \
+  }                                                                            \
+  cerr << "\n";
+
 typedef long long ll;
 typedef pair<int, int> ipair;
 
@@ -23,10 +35,11 @@ void solve() {}
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  int t;
-  cin >> t;
-  for (int i = 0; i < t; i++) {
+  int T;
+  cin >> T;
+  for (int t = 1; t <= T; t++) {
     solve();
+    // cout << "Case #" << t << ": " << ans << "\n";
   }
   return 0;
 }
